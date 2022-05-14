5495ac8af22927f7283e4bfa9cb65c3b66cd085c
daiyongtao <18811508093@163.com>
2022-05-14 22:16:27 +0800

    first commit

---
+++ strategy/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  strategy
+//
+//  Created by 花果山松鼠 on 2022/5/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
